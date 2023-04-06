c09c0d585b1b56071840d4f262cb429b8e4966d2
Sandra S <zandrastr@hotmail.com>
2023-04-06 20:53:19 +0200

    install tinymce editor

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,10 @@
-console.log('Test');
+tinymce.init({
+    selector: '#noteContent',
+    plugins: 'code',
+    toolbar: 'undo redo | forecolor backcolor | bold italic',
+    setup: function(editor){
+        editor.on('change', function(){
+            editor.save();
+        })
+    }
+})
