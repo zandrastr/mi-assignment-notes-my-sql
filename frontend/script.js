7e30bd216044c925a76038a22dc915c6517cc7ff
Sandra S <zandrastr@hotmail.com>
2023-04-07 21:35:33 +0200

    Add function to create login form

--- frontend/script.js
+++ frontend/script.js
@@ -9,10 +9,52 @@ tinymce.init({
     }
 })
 
-const saveBtn = document.getElementById('saveBtn');
-const noteContent = document.getElementById('noteContent');
-const noteContentResult = document.getElementById('noteContentResult');
+let root = document.getElementById('root');
 
-saveBtn.addEventListener('click', function(){
-    noteContentResult.innerHTML = noteContent.value;
-})
+//Function to create login form
+function printLoginForm() {
+    const loginEmail = document.createElement('input');
+    const loginPassword = document.createElement('input');
+    const loginBtn = document.createElement('button');
+
+    loginEmail.id = 'loginEmail';
+    loginPassword.id = 'loginPassword';
+    loginBtn.id = 'loginBtn';
+
+    loginEmail.class = 'loginEmail';
+    loginPassword.class = 'loginPassword';
+    loginBtn.class = 'loginBtn';
+
+    loginEmail.placeholder = 'Email';
+    loginPassword.placeholder = 'Password';
+    loginBtn.innerText = 'Login';
+
+    root.append(loginEmail, loginPassword, loginBtn);
+
+    loginBtn.addEventListener('click', () => {
+        let loginInput = {
+            email: loginEmail.value,
+            password: loginPassword.value
+        }
+
+         console.log('Login input:', loginInput);
+    
+        //TO DO 
+        //Add Fetch, post to backend server
+        //If login not ok: show error message
+        //If login ok: print notes
+
+        //Clear root element
+        root.innerHTML = '';
+
+        //Call function to print notes list
+        printNotesList();
+    })
+}
+
+//Function to print notes list
+function printNotesList() {
+    console.log('Function to print notes list');
+}
+
+printLoginForm();
